04803f098dbaf715fc88834859c8dd908a020098
Yusuf Abdulmuiz <120418140+Yusufabdulmuiz@users.noreply.github.com>
2023-06-19 21:43:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,202 @@
+var _gaq = _gaq || [];
+
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+//to get value of input
+var nameInput = document.querySelector('input');
+
+
+var btn = document.querySelector('button');
+var Main = document.querySelector('main');
+
+btn.addEventListener('click', function() {
+  if (nameInput.value == "") {
+   btn.removeAttribute('id','startConfetti');
+    alert('enter your name');
+   
+
+  }
+  else if( nameInput.value == 'Hasan' || nameInput.value== 'Sufyan' || nameInput.value== 'Bello' || nameInput.value== 'Toyyeb' || nameInput.value== 'Olaitan' ){
+    btn.setAttribute('id','startConfetti')
+   
+   Main.innerHTML = '<audio autoplay loop ><source src="/media/Eid-Audio.mp3"></audio><div  class="box"><span class="name"></span><span class = "eid">Eid mubarak in advance, Beloved brother!</span><p>May <span>Allah </span> spare our lives! Ameen.</p></div>     <h5>greetings from: <span>abdulmuiz</span></h5><div class="days-left"><div><p id="days" >00</p><span>Days</span></div> <div><p id="hours">00</p><span>Hours</span></div><div><p id="minutes">00</p><span>Minutes</span></div><div><p id="seconds">00</p><span>Seconds</span> </div></div>';
+    
+    
+   //countdown
+
+  var countDown = new Date("June 28, 2023 00:00:00").getTime();
+  var i = setInterval(() => {
+    var now = new Date().getTime();
+    var difference = countDown - now;
+  
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000 );
+  
+  
+  
+    document.querySelector('#days').innerHTML = days;
+    document.querySelector('#hours').innerHTML = hours;
+    document.querySelector('#minutes').innerHTML = minutes;
+    document.querySelector('#seconds').innerHTML = seconds;
+  
+  
+    if (difference < 0) {
+      clearInterval(i);
+      document.querySelector('#days').innerHTML = '00';
+      document.querySelector('#hours').innerHTML = '00';
+      document.querySelector('#minutes').innerHTML = '00';
+      document.querySelector('#seconds').innerHTML = '00';
+    }
+  
+  }, 1000);
+  
+  
+
+
+   
+   
+   var nameSpan = document.querySelector('span');
+   
+   nameSpan.innerHTML = `${nameInput.value},`;
+  }
+
+
+  else if( nameInput.value == 'Faheedah' || nameInput.value== 'faheedah' || nameInput.value== 'Haliyah' || nameInput.value== 'Umm aslam' || nameInput.value== 'zulaikha' ){
+    btn.setAttribute('id','startConfetti')
+   
+   Main.innerHTML = '<audio autoplay loop ><source src="/media/Eid-Audio.mp3"></audio><div  class="box"><span class="name"></span><span class = "eid">Eid mubarak in advance, Beloved sister!</span><p>May <span>Allah </span> spare our lives! Ameen.</p></div>     <h5>greetings from: <span>abdulmuiz</span></h5><div class="days-left"><div><p id="days" >00</p><span>Days</span></div> <div><p id="hours">00</p><span>Hours</span></div><div><p id="minutes">00</p><span>Minutes</span></div><div><p id="seconds">00</p><span>Seconds</span> </div></div>';
+    
+    
+   //countdown
+
+  var countDown = new Date("June 28, 2023 00:00:00").getTime();
+  var i = setInterval(() => {
+    var now = new Date().getTime();
+    var difference = countDown - now;
+  
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000 );
+  
+  
+  
+    document.querySelector('#days').innerHTML = days;
+    document.querySelector('#hours').innerHTML = hours;
+    document.querySelector('#minutes').innerHTML = minutes;
+    document.querySelector('#seconds').innerHTML = seconds;
+  
+  
+    if (difference < 0) {
+      clearInterval(i);
+      document.querySelector('#days').innerHTML = '00';
+      document.querySelector('#hours').innerHTML = '00';
+      document.querySelector('#minutes').innerHTML = '00';
+      document.querySelector('#seconds').innerHTML = '00';
+    }
+  
+  }, 1000);
+  
+  
+
+
+   
+   
+   var nameSpan = document.querySelector('span');
+   
+   nameSpan.innerHTML = `${nameInput.value},`;
+  }
+  else {
+    btn.setAttribute('id','startConfetti')
+    Main.innerHTML = '<audio autoplay loop ><source src="/media/Eid-Audio.mp3"></audio><div  class="box"><span class="name"></span><span class = "eid">Eid mubarak in advance!</span><p>May <span>Allah </span> spare our lives! Ameen.</p></div>     <h5>greetings from: <span>abdulmuiz</span></h5><div class="days-left"><div><p id="days" >00</p><span>Days</span></div> <div><p id="hours">00</p><span>Hours</span></div><div><p id="minutes">00</p><span>Minutes</span></div><div><p id="seconds">00</p><span>Seconds</span> </div></div>'
+    
+    
+    
+    var nameSpan= document.querySelector('span');
+
+  nameSpan.innerHTML = `${nameInput.value},`;
+
+//countdown
+
+  var countDown = new Date("June 28, 2023 00:00:00").getTime();
+  var i = setInterval(() => {
+    var now = new Date().getTime();
+    var difference = countDown - now;
+  
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000 );
+  
+  
+  
+    document.querySelector('#days').innerHTML = days;
+    document.querySelector('#hours').innerHTML = hours;
+    document.querySelector('#minutes').innerHTML = minutes;
+    document.querySelector('#seconds').innerHTML = seconds;
+  
+  
+    if (difference < 0) {
+      clearInterval(i);
+      document.querySelector('#days').innerHTML = '00';
+      document.querySelector('#hours').innerHTML = '00';
+      document.querySelector('#minutes').innerHTML = '00';
+      document.querySelector('#seconds').innerHTML = '00';
+    }
+  
+  }, 1000);
+  
+  
+
+
+  }
+});
+
+
+//countdown
+var countDown = new Date("June 28, 2023 00:00:00").getTime();
+var i = setInterval(() => {
+  var now = new Date().getTime();
+  var difference = countDown - now;
+
+  var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((difference % (1000 * 60)) / 1000 );
+
+
+
+  document.querySelector('#days').innerHTML = days;
+  document.querySelector('#hours').innerHTML = hours;
+  document.querySelector('#minutes').innerHTML = minutes;
+  document.querySelector('#seconds').innerHTML = seconds;
+
+
+  if (difference < 0) {
+    clearInterval(i);
+    document.querySelector('#days').innerHTML = '00';
+    document.querySelector('#hours').innerHTML = '00';
+    document.querySelector('#minutes').innerHTML = '00';
+    document.querySelector('#seconds').innerHTML = '00';
+  }
+
+}, 1000);
+
+
+
+
+//if ( difference ) {
+
+//}
